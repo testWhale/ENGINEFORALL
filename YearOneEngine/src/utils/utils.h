735591fbea49c14a8770298e552d7dbf25961e76
testWhale <uyaitau@outlook.com>
2025-10-16 23:16:56 +0800

    w
    w

--- YearOneEngine/src/utils/utils.h
+++ YearOneEngine/src/utils/utils.h
@@ -1,6 +1,7 @@
 #ifndef UTIL_H
 #define UTIL_H
 #include "cprocessing.h"
+
 int IsAreaClicked(float area_center_x, float area_center_y, float area_width, float area_height, float click_x, float click_y, int button);
 int IsCircleClicked(float circle_center_x, float circle_center_y, float diameter, float click_x, float click_y);
 
