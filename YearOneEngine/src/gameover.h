6997daf7c732d76e18be42246f1e71995d8df311
xavier080802 <2502322@sit.singaporetech.edu.sg>
2025-10-28 16:30:27 +0800

    gameover
    blah blah

---
+++ YearOneEngine/src/gameover.h
@@ -0,0 +1,16 @@
+
+#ifndef TEST_H
+#define TEST_H
+// Init, Set currState, send function pointer to States.init
+ // forward declarations
+
+ // Pass data you want to show on the screen.
+void GameOver_SetData(float finalTime, int goals);
+
+void Test_Init(void);
+
+void Test_Update(void);
+
+void Test_Exit(void);
+
+#endif
