498f9609b1ea24dc5a2df95be7fee786985e15da
QuinnQuak <quakjunehao@gmail.com>
2025-11-09 00:46:44 +0800

    Dynamic Econ Code

--- YearOneEngine/src/economy/economyCode.h
+++ YearOneEngine/src/economy/economyCode.h
@@ -2,8 +2,10 @@ char moneyString[10];
 char statisticString[100];
 char statisticString2[100];
 char clicker1Cost[100],clicker2Cost[100];
+char troop1Cost[100], troop2Cost[100], troop3Cost[100];
 extern float currentMoney, passiveIncome;
 int clickerUpgrade1Count,clickerUpgrade2Count;
+int troop1Count, troop2Count, troop3Count;
 
 
 int Purchase_System(float* currentMoney, float itemCost);
